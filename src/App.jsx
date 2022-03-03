6b6a0e960540e80d1305868c3ca8e39fc0a28d73
nehachatse <nehachatse02@gmail.com>
2022-03-03 16:01:44 +0530

    clearing branch

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import './App.css';
+import { Navbar } from './Components/Navbar/Navbar';
 
 function App() {
   return (
-    <div className="App">
-     
+    <div>
+      <Navbar/>
     </div>
   );
 }
