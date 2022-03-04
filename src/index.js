30a50deb7854e888ef08faa32a5e7cf6a22f8cf2
nehachatse <nehachatse02@gmail.com>
2022-03-04 13:27:31 +0530

    conflict solve

--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import ProductContext, { ProductContextProvider } from './Context/ProductContext';
+import { ProductContextProvider } from './Context/ProductContext';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
     <ProductContextProvider>
+    <BrowserRouter>
       <App />
+    </BrowserRouter>
     </ProductContextProvider>
   </React.StrictMode>,
   document.getElementById('root')
