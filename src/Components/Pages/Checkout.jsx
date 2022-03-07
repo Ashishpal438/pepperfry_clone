a37f6f0559bdecb6d96f7d6dfadd9af1e4f3b19c
Ashish kumar Pal <ashishpal438@gmail.com>
2022-03-07 21:30:09 +0530

    payment

--- src/Components/Pages/Checkout.jsx
+++ src/Components/Pages/Checkout.jsx
@@ -1,9 +1,11 @@
 import styles from './Checkout.module.css';
 import { VscLocation } from 'react-icons/vsc';
 import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom'
 import CartItem from './CartItem';
 
 export default function Checkout() {
+      const navigate = useNavigate();
     const [cart, setCart] = useState([]);
     const [cartValue, setCartValue] = useState(0);
     const [discountValue, setDiscountValue] = useState(0);
@@ -102,7 +104,7 @@ export default function Checkout() {
                         <input type="checkbox" name='gov' onChange={(e) => handleChecked(e)} /><label>Contribute Rs.99 For COVID Relief Through GiveIndia.</label>
                     </div>
 
-                    <button className={styles.btn}>PLACE ORDER</button>
+                    <button className={styles.btn} onClick={() => navigate('/')} >PLACE ORDER</button>
                 </div>
             </div>
         </div>
